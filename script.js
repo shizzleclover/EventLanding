9d75d169aebe828e7e3c13092d8e6b364b55ee89
Murewa Ajala <murewatajala@gmail.com>
2024-07-10 09:02:14 +0100

    Loading animation and extra screen

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function submitForm(event) {
     event.preventDefault();
     const form = event.target;
     const formData = new FormData(form);
-    const url = 'https://script.google.com/macros/s/AKfycbyiG89Q2t03eWP3XB6P4YX32pRL_p54f6yFFrl1htInUw6JL1u5QZBbn2Por1lInV56RQ/exec'; // Replace with your Web App URL
+    const url = 'https://script.google.com/macros/s/AKfycbyiG89Q2t03eWP3XB6P4YX32pRL_p54f6yFFrl1htInUw6JL1u5QZBbn2Por1lInV56RQ/exec'; 
+
+    
+    const overlay = document.getElementById('overlay');
+    const spinner = document.querySelector('.spinner');
+    
+    overlay.style.display = 'flex';
+    spinner.style.display = 'block';
 
     fetch(url, {
         method: 'POST',
@@ -10,12 +17,13 @@ function submitForm(event) {
     })
     .then(response => response.text())
     .then(result => {
-        alert(result);
-        form.reset(); // Optionally reset the form after successful submission
+        form.reset(); 
+        window.location.href = 'success.html';
     })
     .catch(error => {
         console.error('Error:', error);
         alert('An error occurred while submitting the form.');
+        overlay.style.display = 'none';
     });
 }
 
